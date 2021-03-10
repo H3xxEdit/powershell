75a59c4a604a399265f07f7bc7c9256a01d69954
H3xx <h3xx@live.com>
2021-03-10 12:40:39 -0500

    Updated

--- filelist/list_files.ps1
+++ filelist/list_files.ps1
@@ -1,4 +1,4 @@
 Get-ChildItem C:\ -recurse -ErrorAction SilentlyContinue |
 	select-object FullName,Length,Mode,CreationTimeUtc,LastAccessTimeUtc,LastWriteTimeUtc,Attributes |
-	Export-Csv $env:HOMEDRIVE$env:HOMEPATH\Desktop\fl-$env:COMPUTERNAME.csv
+	Export-Csv $env:HOMEDRIVE$env:HOMEPATH\Desktop\filelist-$env:COMPUTERNAME.csv
 	
