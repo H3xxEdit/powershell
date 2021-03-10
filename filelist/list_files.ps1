7559148a520716d3d705850cc6f5bfcfa39f6a5a
H3xx <h3xx@live.com>
2021-03-10 14:04:08 -0500

    Updated

--- filelist/list_files.ps1
+++ filelist/list_files.ps1
@@ -1,4 +1,6 @@
 Get-ChildItem C:\ -recurse -ErrorAction SilentlyContinue |
+	where-object -FilterScript {$_.LastWriteTime -gt '2019-01-01'} |
+	sort LastWriteTime -Descending |
 	select-object FullName,Length,Mode,CreationTimeUtc,LastAccessTimeUtc,LastWriteTimeUtc,Attributes |
-	Export-Csv $env:HOMEDRIVE$env:HOMEPATH\Desktop\filelist-$env:COMPUTERNAME.csv
+	Export-Csv $env:HOMEDRIVE$env:HOMEPATH\Desktop\fl-$env:COMPUTERNAME.csv
 	
